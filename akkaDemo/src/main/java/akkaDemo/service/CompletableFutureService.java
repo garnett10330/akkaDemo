0fdf8271ae7cd3c212bdc037855325b8e90b3aa7
sirenia10330@gmail.com <sirenia10330@gmail.com>
2018-04-10 11:04:21 +0800

    commit akka demo 第一版

---
+++ akkaDemo/src/main/java/akkaDemo/service/CompletableFutureService.java
@@ -0,0 +1,27 @@
+package akkaDemo.service;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akkaDemo.di.SpringExtension;
+import akkaDemo.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class CompletableFutureService {
+
+    @Autowired
+    private ActorSystem actorSystem;
+
+    @Autowired
+    private SpringExtension springExtension;
+
+    public CompletableFuture<Message> get(String payload, Long id) {
+        CompletableFuture<Message> future = new CompletableFuture<>();
+        ActorRef workerActor = actorSystem.actorOf(springExtension.props("workerActor", future), "worker-actor");
+        workerActor.tell(new Message(payload, id), null);
+        return future;
+    }
+}
